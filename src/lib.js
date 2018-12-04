d3f0a61b95c99f65a3fa6d8764162dd015ae7743
naman Sharma <44020825+namankrs@users.noreply.github.com>
2018-12-04 10:47:25 +0530

    Introduce getLines().
    It takes a string and count of lines as argument and returns a
    substring.

--- src/lib.js
+++ src/lib.js
@@ -2,4 +2,9 @@ const getCharacters = function(string,count){
   return string.slice(0,count);
 }
 
-module.exports = {getCharacters};
+const getLines = function(string,count){
+  return string.split('\n').slice(0,count).join('\n');
+}
+
+module.exports = {getCharacters,
+  getLines};
--- test/libTest.js
+++ test/libTest.js
@@ -1,8 +1,9 @@
 let {equal,deepEqual} = require('assert');
-let {getCharacters} = require('../src/lib.js');
+let {getCharacters,
+  getLines} = require('../src/lib.js');
 
 describe('getCharacters',function(){
-  it('should return a empty string when count is given as 0',function(){
+  it('should return an empty string when count is given as 0',function(){
     equal(getCharacters('naman',0),'');
   })
   it('should return a set of characters when count greater than 0 is given',function(){
@@ -10,5 +11,21 @@ describe('getCharacters',function(){
   })
   it('should return the whole string if count greater than string length is given',function(){
     equal(getCharacters('naman',10),'naman');
+  })
+})
+
+describe('getLines',function(){
+  let input = 'there are\nfew fruits in \n the orchard.'
+  it('should return an empty string when count is given as 0',function(){
+    equal(getLines(input,0),'');
+  })
+  it('should return the number of lines as specified by count',function(){
+    let expectedOutput = 'there are\nfew fruits in ' 
+    equal(getLines(input,2),expectedOutput);
+    equal(getLines(input,1),'there are');
+  })
+  it('should return whole string if specified count is more than the number of lines in string',function(){
+  let expectedOutput = 'there are\nfew fruits in \n the orchard.'
+    equal(getLines(input,10),expectedOutput);
   })
 })
