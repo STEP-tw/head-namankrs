a74521852aad9e84173f99e6687e8e5fc6d5b825
naman sharma <44020825+namankrs@users.noreply.github.com>
2018-12-15 19:33:43 +0530

    changed let to const

--- tail.js
+++ tail.js
@@ -1,22 +1,7 @@
 const main = function() {
-  let fs = require("fs");
-  let { tail } = require("./src/lib.js");
-  let inputs = process.argv;
+  const fs = require("fs");
+  const { tail } = require("./src/lib.js");
+  const inputs = process.argv;
   console.log(tail(fs, inputs.slice(2)));
 };
 main();
-/* 
-  Usage:
-  node ./tail.js file1
-  node ./tail.js -n5 file1
-  node ./tail.js -n 5 file1
-  node ./tail.js -5 file1
-  node ./tail.js file1 file2
-  node ./tail.js -n 5 file1 file2
-  node ./tail.js -n5 file1 file2
-  node ./tail.js -5 file1 file2 
-  node ./tail.js -c5 file1
-  node ./tail.js -c 5 file1
-  node ./tail.js -c5 file1 file2
-  node ./tail.js -c 5 file1 file2
-*/
