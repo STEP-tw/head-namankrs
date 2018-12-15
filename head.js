d7226f3d0c9267d8a058f819ab0cc47719f49d36
naman sharma <44020825+namankrs@users.noreply.github.com>
2018-12-15 19:28:37 +0530

    removed commented lines

--- head.js
+++ head.js
@@ -1,24 +1,8 @@
-let fs = require("fs");
-let { head } = require("./src/lib.js");
+const fs = require("fs");
+const { head } = require("./src/lib.js");
 
 const main = function() {
   console.log(head(fs, process.argv.slice(2)));
 };
 
 main();
-
-/* 
-  Usage:
-  node ./head.js file1
-  node ./head.js -n5 file1
-  node ./head.js -n 5 file1
-  node ./head.js -5 file1
-  node ./head.js file1 file2
-  node ./head.js -n 5 file1 file2
-  node ./head.js -n5 file1 file2
-  node ./head.js -5 file1 file2 
-  node ./head.js -c5 file1
-  node ./head.js -c 5 file1
-  node ./head.js -c5 file1 file2
-  node ./head.js -c 5 file1 file2
-  */
