8d1b0754a6b400a8b18bbd660b25b408c99caf76
naman sharma <44020825+namankrs@users.noreply.github.com>
2018-12-16 12:59:13 +0530

    modified tests for parseInput.js

--- test/parseInputTest.js
+++ test/parseInputTest.js
@@ -2,45 +2,56 @@ const assert = require("assert");
 const { parseInput } = require("../src/parseInput.js");
 describe("parseInput", function() {
   it("should return the default state when no option and count are provided", function() {
-    assert.deepEqual(parseInput(["head.js"]), {
+    const expectedOutput = {
       option: "-n",
       count: "10",
-      files: ["head.js"]
-    });
+      files: ["../file"]
+    };
+    assert.deepEqual(parseInput(["../file"]), expectedOutput);
   });
-  it("should change the default when -n option is given", function() {
-    assert.deepEqual(parseInput(["-n", "5", "hello", "world"]), {
+  it("should change the default when -n option is given with count", function() {
+    const input = ["-n", "13", "../file"];
+    const expectedOutput = {
       option: "-n",
-      count: "5",
-      files: ["hello", "world"]
-    });
+      count: "13",
+      files: ["../file"]
+    };
+    assert.deepEqual(parseInput(input), expectedOutput);
   });
-  it("should change the default when -c option is given", function() {
-    assert.deepEqual(parseInput(["-c", "5", "hello", "world"]), {
+  it("should change the default when -c option is given with count", function() {
+    const input = ["-c", "5", "../file1", "../file2"];
+    const expectedOutput = {
       option: "-c",
       count: "5",
-      files: ["hello", "world"]
-    });
+      files: ["../file1", "../file2"]
+    };
+    assert.deepEqual(parseInput(input), expectedOutput);
   });
   it("should change the default when option and count are not seperated by space", function() {
-    assert.deepEqual(parseInput(["-c5", "hello", "world"]), {
+    const input = ["-c5", "../file1", "../file2"];
+    let expectedOutput = {
       option: "-c",
       count: "5",
-      files: ["hello", "world"]
-    });
+      files: ["../file1", "../file2"]
+    };
+    assert.deepEqual(parseInput(input), expectedOutput);
   });
   it("should take -n as default when only count is given", function() {
-    assert.deepEqual(parseInput(["-5", "hello", "world"]), {
+    const input = ["-5", "../file1", "../file2"];
+    const expectedOutput = {
       option: "-n",
       count: "5",
-      files: ["hello", "world"]
-    });
+      files: ["../file1", "../file2"]
+    };
+    assert.deepEqual(parseInput(input), expectedOutput);
   });
   it("should return the default count if no count is given", function() {
-    assert.deepEqual(parseInput(["hello", "world"]), {
+    const input = ["../file1", "../file2"];
+    const expectedOutput = {
       option: "-n",
       count: "10",
-      files: ["hello", "world"]
-    });
+      files: ["../file1", "../file2"]
+    };
+    assert.deepEqual(parseInput(input), expectedOutput);
   });
 });
