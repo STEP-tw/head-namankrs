79cb97ef8472a11a90cece3f2f32dbaa9582c6db
naman sharma <44020825+namankrs@users.noreply.github.com>
2018-12-19 10:43:21 +0530

    Integrate inputValidatorTest.js in parserTest.js

--- test/parserTest.js
+++ test/parserTest.js
@@ -1,5 +1,6 @@
 const assert = require("assert");
-const { parseInput } = require("../src/parser.js");
+const { parseInput, validateInput } = require("../src/parser.js");
+
 describe("parseInput", function() {
   it("should return the default state when no option and count are provided", function() {
     const expectedOutput = {
@@ -53,5 +54,56 @@ describe("parseInput", function() {
       files: ["../file1", "../file2"]
     };
     assert.deepEqual(parseInput(input), expectedOutput);
+  });
+});
+
+describe("validateInput", function() {
+  it("should return error message when -0 is given as option", function() {
+    const input = ["-0", "head.js"];
+    const expectedOutput = {
+      message: "head: illegal line count -- 0",
+      errorState: true
+    };
+    assert.deepEqual(validateInput(input), expectedOutput);
+  });
+  it("should return a error message when option is other than n or c", function() {
+    const input = ["-v", "5", "head.js"];
+    const expectedOutput = {
+      message: `head: illegal option -- v\nusage: head [-n lines | -c bytes] [file ...]`,
+      errorState: true
+    };
+    assert.deepEqual(validateInput(input), expectedOutput);
+  });
+  it("should return error message with first letter of the word given as wrong option", function() {
+    const input = ["-hello", "5", "head.js"];
+    const expectedOutput = {
+      message: `head: illegal option -- h\nusage: head [-n lines | -c bytes] [file ...]`,
+      errorState: true
+    };
+    assert.deepEqual(validateInput(input), expectedOutput);
+  });
+  it("should return a error message when count is given as 0", function() {
+    const input = ["-n0", "6", "head.js"];
+    const expectedOutput = {
+      message: "head: illegal line count -- 0",
+      errorState: true
+    };
+    assert.deepEqual(validateInput(input), expectedOutput);
+  });
+  it("should return a error message for alphanumeric option merged count", function() {
+    const input = ["-n3av", "head.js"];
+    const expectedOutput = {
+      message: "head: illegal line count -- 3av",
+      errorState: true
+    };
+    assert.deepEqual(validateInput(input), expectedOutput);
+  });
+  it("should return a error message for alphanumeric count", function() {
+    const input = ["-n", "3av", "head.js"];
+    const expectedOutput = {
+      message: "head: illegal line count -- 3av",
+      errorState: true
+    };
+    assert.deepEqual(validateInput(input), expectedOutput);
   });
 });
